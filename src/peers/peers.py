c964910b2140e255d78f8e583cf0c976e3dec858
CpulsiveK <impulsivecliff7@gmail.com>
2023-02-18 16:41:51 -0800

    added a line to send the request type along with the number of files

--- src/peers/peers.py
+++ src/peers/peers.py
@@ -5,22 +5,24 @@ import socket
 class Client:
     def makeFilePublic(self, file:list[File]):
         number_of_files = len(file)
+        request_type = "makeFilePublic"
 
         # create a socket and send the number of files the server should expect
         try:
             p_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             p_sock.connect((SERVER_ADDR, PORT))
             print("connecting...")
+            p_sock.send(request_type.encode())
             p_sock.send(str(number_of_files).encode())
-        except socket.error as e:
-            print("Couldn't create socket due to ", e)
+        except socket.error as perror:
+            print("Couldn't create socket due to ", perror)
         
         for i in range(len(file)):
             try:
                 p_sock.send(str(file[i].file_name).encode())
                 p_sock.send(str(file[i].file_size).encode())
-            except socket.error as e:
-                print("Could not send data due to ", e)
+            except socket.error as perror:
+                print("Could not send data due to ", perror)
 
 file = [File("COE 152", 23), File("Computer Networks", 45)]
 
